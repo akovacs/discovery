64ac9a2d5769e72506334a350a306c3180d62987
Arpad Kovacs <akovacs@cs.stanford.edu>
2018-03-25 19:42:04 -0700

    Implement led roulette

--- src/05-led-roulette/src/main.rs
+++ src/05-led-roulette/src/main.rs
@@ -9,13 +9,25 @@ use aux5::{Delay, Leds};
 fn main() {
     let (mut delay, mut leds): (Delay, Leds) = aux5::init();
 
-    let half_period = 500_u16;
+    let step_period = 50_u16;
+    let mut step = 0;
+    let intervals = [(0,2), (2,4), (4,6), (6,8), (8,10), (10,12), (12,14)];
 
     loop {
-        leds[0].on();
-        delay.delay_ms(half_period);
-
-        leds[0].off();
-        delay.delay_ms(half_period);
+        if step == 0 || step == 14 || step == 15 {
+            leds[0].on();
+        } else {
+            leds[0].off();
+        }
+        let index = 1;
+        for (index, &(start, stop)) in intervals.iter().enumerate() {
+            if step >= start && step <= stop{
+                leds[index+1].on();
+            } else {
+                leds[index+1].off();
+            }
+        }
+        delay.delay_ms(step_period);
+        step = (step + 1) % 16;
     }
 }
