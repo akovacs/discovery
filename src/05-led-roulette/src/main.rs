25dc0ef4da7efc9039ab99f049d220a13199a04e
Arpad Kovacs <akovacs@cs.stanford.edu>
2018-03-25 19:54:47 -0700

    More efficient implementation of led roulette

--- src/05-led-roulette/src/main.rs
+++ src/05-led-roulette/src/main.rs
@@ -10,24 +10,13 @@ fn main() {
     let (mut delay, mut leds): (Delay, Leds) = aux5::init();
 
     let step_period = 50_u16;
-    let mut step = 0;
-    let intervals = [(0,2), (2,4), (4,6), (6,8), (8,10), (10,12), (12,14)];
-
     loop {
-        if step == 0 || step == 14 || step == 15 {
-            leds[0].on();
-        } else {
-            leds[0].off();
+        for current in 0..8 {
+            let next = (current + 1) % 8;
+            leds[next].on();
+            delay.delay_ms(step_period);
+            leds[current].off();
+            delay.delay_ms(step_period);
         }
-        let index = 1;
-        for (index, &(start, stop)) in intervals.iter().enumerate() {
-            if step >= start && step <= stop{
-                leds[index+1].on();
-            } else {
-                leds[index+1].off();
-            }
-        }
-        delay.delay_ms(step_period);
-        step = (step + 1) % 16;
     }
 }
