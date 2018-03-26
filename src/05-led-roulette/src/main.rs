58e7dc6e6e83b0af993ddcad9324fb514e413c2e
Arpad Kovacs <akovacs@cs.stanford.edu>
2018-03-25 18:54:37 -0700

    Add simple blinking led program using Leds and Delay from aux5 crate

--- src/05-led-roulette/src/main.rs
+++ src/05-led-roulette/src/main.rs
@@ -3,11 +3,19 @@
 
 extern crate aux5;
 
-fn main() {
-    let _y;
-    let x = 42;
-    _y = x;
+use aux5::prelude::*;
+use aux5::{Delay, Leds};
 
-    // infinite loop; just so we don't leave this stack frame
-    loop {}
+fn main() {
+    let (mut delay, mut leds): (Delay, Leds) = aux5::init();
+
+    let half_period = 500_u16;
+
+    loop {
+        leds[0].on();
+        delay.delay_ms(half_period);
+
+        leds[0].off();
+        delay.delay_ms(half_period);
+    }
 }
