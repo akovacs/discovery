130d43057014cc1e0cd3f4c5e5431b782b7bc7fa
Arpad Kovacs <akovacs@cs.stanford.edu>
2018-03-28 21:37:14 -0700

    Use typesafe peripheral api

--- src/07-registers/src/main.rs
+++ src/07-registers/src/main.rs
@@ -2,25 +2,18 @@
 
 extern crate aux7;
 
-use core::ptr;
-
 fn main() {
-    aux7::init();
+    let gpioe = aux7::init().1;
 
-    unsafe {
-        // A magic address!
-        const GPIOE_BSRR: u32 = 0x48001018;
+    // Turn on the North LED
+    gpioe.bsrr.write(|w| w.bs9().set_bit());
 
-        // Turn on the "North" LED (red)
-        ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << 9);
+    // Turn on the East LED
+    gpioe.bsrr.write(|w| w.bs11().set_bit());
 
-        // Turn on the "East" LED (green)
-        ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << 11);
+    // Turn off the North LED
+    gpioe.bsrr.write(|w| w.br9().set_bit());
 
-        // Turn off the "North" LED
-        ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << (9 + 16));
-
-        // Turn off the "East" LED
-        ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << (11 + 16));
-    }
+    // Turn off the East LED
+    gpioe.bsrr.write(|w| w.br11().set_bit());
 }
