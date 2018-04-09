6efb0ae0c0274db1832c49d8f8717ceeed93c7ff
Arpad Kovacs <akovacs@cs.stanford.edu>
2018-04-08 22:25:43 -0700

    Alternating LEDs with accelerating/decelerating speed

--- src/05-led-roulette/src/main.rs
+++ src/05-led-roulette/src/main.rs
@@ -9,18 +9,28 @@ use aux5::{Delay, Leds};
 fn main() {
     let (mut delay, mut leds): (Delay, Leds) = aux5::init();
 
-    let step_period = 100_u16;
-    let mut step = 0;
+    let mut step_period = 100.0;
+    let mut decreasing = true;
     let intervals = [(5,1), (1,6), (6,2), (2,7), (7,3), (3,0), (0,4), (4,1),
       (1,5), (5,2), (2,6), (6,3), (3,7), (7,4), (4,0), (0,5)];
-
     loop {
         for &(off, on) in intervals.iter() {
             leds[off].off();
-            delay.delay_ms(step_period);
+            let wait = step_period as u16;
+            delay.delay_ms(wait);
             leds[on].on();
-            delay.delay_ms(step_period);
-            delay.delay_ms(step_period);
+            delay.delay_ms(wait);
+            delay.delay_ms(wait);
+            if (step_period <= 20.0) {
+                decreasing = false
+            } else if (step_period >= 100.0) {
+                decreasing = true
+            }
+            if decreasing {
+                step_period = step_period * 0.99;
+            } else {
+                step_period = step_period * 1.01;
+            }
         }
     }
 }
