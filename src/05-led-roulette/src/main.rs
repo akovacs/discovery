2c05540ce86f5c373bd8d10e0f6cf8a464dfb2b9
Arpad Kovacs <akovacs@cs.stanford.edu>
2018-04-08 21:54:03 -0700

    LEDs on alternating sides pattern

--- src/05-led-roulette/src/main.rs
+++ src/05-led-roulette/src/main.rs
@@ -9,13 +9,17 @@ use aux5::{Delay, Leds};
 fn main() {
     let (mut delay, mut leds): (Delay, Leds) = aux5::init();
 
-    let step_period = 50_u16;
+    let step_period = 100_u16;
+    let mut step = 0;
+    let intervals = [(5,1), (1,6), (6,2), (2,7), (7,3), (3,0), (0,4), (4,1),
+      (1,5), (5,2), (2,6), (6,3), (3,7), (7,4), (4,0), (0,5)];
+
     loop {
-        for current in 0..8 {
-            let next = (current + 1) % 8;
-            leds[next].on();
+        for &(off, on) in intervals.iter() {
+            leds[off].off();
             delay.delay_ms(step_period);
-            leds[current].off();
+            leds[on].on();
+            delay.delay_ms(step_period);
             delay.delay_ms(step_period);
         }
     }
